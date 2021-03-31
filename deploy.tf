e2a6946a91067850c1de7167bd2436f82f620be1
Osama Emam <emam.osama.contact@gmail.com>
2021-03-31 17:46:26 +0200

    first commit

---
+++ deploy.tf
@@ -0,0 +1,73 @@
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "dc1"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore1"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "cluster1"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "network1"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  count            = 1
+  name             = "master-${count.index}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 4096
+  guest_id = "other3xDebian64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  count            = 2
+  name             = "workers-${count.index}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 2048
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
+
+resource "vsphere_compute_cluster_vm_group" "cluster_vm_group" {
+  name                = "terraform-test-cluster-vm-group"
+  compute_cluster_id  = data.vsphere_compute_cluster.cluster.id
+  virtual_machine_ids = vsphere_virtual_machine.vm.*.id
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+vsphere_user = "root"
+vsphere_password = "THVuREkxKTI+"
+vsphere_server = "192.168.10.10"
